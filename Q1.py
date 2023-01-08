d2f080a5890d3ddba5338b50860c5b3ecbbe7a35
faris771 <farisabufarha33@gmail.com>
2023-01-08 03:54:49 +0200

    Q1_B modified -cos were missing- still needs to be checked

--- Q1.py
+++ Q1.py
@@ -7,18 +7,23 @@ ID: 1200546
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Q1_C
-LL = -10  # UPPER LIMIT
-UL = 9  # LOWER LIMIT
+# Q1_B
+LL = 0  # UPPER LIMIT
+UL = 50  # LOWER LIMIT
+
 n = np.arange(LL, UL + 1)
-z = [5 - i % 5 for i in n]
+mean = 0
+variance = 1
+sigma = np.sqrt(variance)
+w = np.random.normal(mean, sigma, size=len(n))
 
+y = [np.cos(0.04 * np.pi * t) + 0.2 * w[t] for t in n]  # APPENDING EACH VALUE OF y[i] into to the list y
 
-plt.grid()
-plt.stem(n, z)
+plt.grid() # TO SHOW THE GRID
+plt.stem(n, y) # TO PLOT THE GRAPH in discrete form
 plt.xlabel('n')
-plt.xticks(n)
-plt.ylabel('Z[n]')
-plt.title('Q1_C')
-plt.savefig("q1_C.png")
+# plt.xticks(n)
+plt.ylabel('y[n]')
+plt.title('Q1_B')
+plt.savefig("q1_B.png") # SAVING THE PLOT AS A PNG FILE
 plt.show()
