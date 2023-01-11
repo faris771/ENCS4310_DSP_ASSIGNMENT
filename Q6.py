e89d31b58036c7c6059c85417deed616509080e7
faris771 <farisabufarha33@gmail.com>
2023-01-12 01:37:03 +0200

    Q7, but needs to be checked

--- Q6.py
+++ Q6.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 # u(n-5)  interval n, delay -5
@@ -18,17 +19,29 @@ LL = -5
 UL = 45
 n = np.arange(LL, UL + 1)
 
-xn = [(unit_step(n, 0)[i+ abs(LL)] - unit_step(n, -10)[i+ abs(LL)]) for i in n]
+xn = [(unit_step(n, 0)[i + -LL] - unit_step(n, -10)[i +-LL]) for i in n]
 plt.stem(n, xn)
 plt.grid()
-# plt.savefig('Q6_X.png')
+plt.xlabel('n')
+plt.ylabel('x[n]')
+plt.savefig('plots/Q6_X.png') # to save in a certain directory
 plt.show()
 
-hn = [(unit_step(n, 0)[i + abs(LL)] * (0.9 ** i)) for i in
+hn = [(unit_step(n, 0)[i + -LL] * (0.9 ** i)) for i in
       n]  # -LL = --5 = + is for the relative index, there's no index -5, but relatively it's same as index 0 for the list
 plt.stem(n, hn)
 plt.grid()
-# plt.savefig('Q6_X.png')
+plt.xlabel('n')
+plt.ylabel('h[n]')
+plt.savefig('plots/Q6_H.png')
+plt.show()
+
+yn = np.convolve(xn, hn)
+plt.stem(yn)
+plt.xlabel('n')
+plt.ylabel('y[n]')
+plt.grid()
+plt.savefig('plots/Q6_Y.png')
 plt.show()
 
 #
---
+++ Q7.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import random
+
+n = np.arange(-3, 3 + 1)
+xn = np.array([3, 11, 7, 0, -1, 4, 2])
+w = np.random.normal(0, 1, size=len(xn))
+
+yn = [xn[i-2] + w[i] for i in n]
+r = np.correlate(xn, yn)
+plt.stem(r)
+plt.grid()
+plt.title('Q7_A')
+plt.xlabel('n')
+plt.ylabel('r[n]')
+plt.yticks()
+plt.show()
+
+## Q7_B
+yn = [xn[i-4] + w[i] for i in n]
+r = np.correlate(xn, yn)
+plt.stem(r)
+plt.grid()
+plt.title('Q7_B')
+plt.xlabel('n')
+plt.ylabel('r[n]')
+plt.yticks()
+plt.show()
+
+
+
